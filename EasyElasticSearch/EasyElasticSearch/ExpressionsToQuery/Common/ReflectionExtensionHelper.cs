fc242d27181f50f947bcf748f671cd49cff116dc
王闯 <515497782@qq.com>
2020-09-28 09:23:15 +0800

    update

--- EasyElasticSearch/EasyElasticSearch/ExpressionsToQuery/Common/ReflectionExtensionHelper.cs
+++ EasyElasticSearch/EasyElasticSearch/ExpressionsToQuery/Common/ReflectionExtensionHelper.cs
@@ -14,16 +14,15 @@ namespace EasyElasticSearch
                     case ExpressionType.Lambda:
                         expr = ((LambdaExpression) expr).Body;
                         break;
-
                     case ExpressionType.Convert:
                         expr = ((UnaryExpression) expr).Operand;
                         break;
-
                     case ExpressionType.MemberAccess:
+                    {
                         var memberExpression = (MemberExpression) expr;
                         var mi = memberExpression.Member;
                         return mi;
-
+                    }
                     default:
                         return null;
                 }
